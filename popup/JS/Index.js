6031eb1f43fe6947636961b56c6ce53d1447ce79
Song <liusong1998@vip.126.com>
2019-08-15 22:53:41 +1200

    Finish 'Internet Status Checking' function

--- popup/JS/Index.js
+++ popup/JS/Index.js
@@ -1,4 +1,28 @@
-console.log("begin");
+
+function Online(callback){
+    let img = new Image();
+    img.src = 'https://www.baidu.com/favicon.ico?_t=' + Date.now(); //will change to google
+    img.onload=function(){
+        if (callback) callback(true)
+    };
+    img.onerror=function(){
+        if (callback) callback(false)
+    };
+}
+
+Online(function(flag){
+    let internet_status_dom = $('#internet_status');
+    if(flag){
+        let css = {"font-size":"1.2em", "color":"blue","text-align":"center"};
+        internet_status_dom.text("Online");
+        internet_status_dom.css(css);
+    }else{
+        let css ={"font-size":"1.2em", "color":"blue", "text-align":"center"};
+        internet_status_dom.text("Offline");
+        internet_status_dom.css(css);
+    }
+});
+
 document.addEventListener("click", function (e) {
     if (!(e.target.tagName === "BUTTON")) {
         console.log(e.target.tagName);
@@ -21,3 +45,5 @@ document.addEventListener("click", function (e) {
         url: chosenPage
     });
 });
+
+
