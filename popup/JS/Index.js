41cc3942902127dcb2c573c5d4ae037b05207adb
Lauren <39478077+SerendipitousUser@users.noreply.github.com>
2019-09-04 16:46:36 +1200

    Created Homepage.html; Added content pages
    Created a homepage (no css currently), Added content pages to test with (currently under assets);

--- popup/JS/Index.js
+++ popup/JS/Index.js
@@ -32,7 +32,7 @@ document.addEventListener("click", function (e) {
     console.log("click button");
 
     let url_download = "http://confocal-manawatu.pbworks.com/w/page/16346911/Top"; // maybe change later
-    let url_view_modify = "http://www.massey.ac.nz/massey/home.cfm";// not true, need to change
+    let url_view_modify = "Homepage.html";
 
     let chosenPage;
     if (e.target.textContent === "Download Pages") {
