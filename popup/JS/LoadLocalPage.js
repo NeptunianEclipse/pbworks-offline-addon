dc5b073aae2981fc72c3f8e49a916d935c3c1212
Liu,Song <liusong1998@vip.126.com>
2019-09-22 20:15:56 +1200

    some unfinished tests

--- popup/JS/LoadLocalPage.js
+++ popup/JS/LoadLocalPage.js
@@ -1,19 +1,21 @@
 $(document).ready(function () {
     //Automatically populates the homepage with the PBWorks 'frontpage'; won't be hardcoded in future
     $.getJSON('../assets/testPages/sample.json', function (localPage) {
-        console.log(localPage);
-        $("#currentPage").html(localPage.html);
-            }
-                
-    )
+            console.log(localPage);
+            $("#currentPage").html(localPage.html);
+        }
+    );
 
     //Header searchbar
     $("#searchHeaderB").click(function (event) {
         var search = document.getElementById("searchHeader").value;
+        console.log(search);
         $("#localPage").text("Search for '" + search + "' returns: ");
         var result = get_data_name(search);
-        console.log(result);
-       
+        result.then((e) =>{
+            console.log(e.target.result);
+        })
+
     });
 
     //Lower searchbar
@@ -22,12 +24,11 @@ $(document).ready(function () {
         $("#localPage").text("Search for '" + search + "' returns: ");
         var result = get_data_name(search);
         console.log(result);
-       
+
     });
 
 });
 
 function displaySearch(list) {
-
 
 }
