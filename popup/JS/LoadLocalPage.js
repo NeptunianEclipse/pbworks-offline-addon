ce7eeede38c0f14671c353b712da4ca1d6eef05b
Lauren <39478077+SerendipitousUser@users.noreply.github.com>
2019-10-02 13:32:00 +1300

    Changed interface and refactored search functions
    - Added edit link to each search result to be formated into button
    - Refactored search functions to reduce repeated code; all now use 'createSearchResults', 'createButtonListeners' and 'displaySearch'
    - Removed redundent page elements, incorporated remaining search bar into the header

--- popup/JS/LoadLocalPage.js
+++ popup/JS/LoadLocalPage.js
@@ -13,12 +13,6 @@ $(document).ready(function () {
 
     });
 
-    //Lower searchbar
-    $("#searchPagesB").click(function (event) {
-        var search = document.getElementById("searchPages").value;
-        searchFor(search);
-
-    });
 
 
     $("#searchOid").click(function (event) {
@@ -29,15 +23,13 @@ $(document).ready(function () {
             var result = get_data_oid(search);
             result.then((e) => {
                 console.log(e.target.result);
-                if (e.target.result === undefined) {
-                    $("#currentPage").text("No pages found!");
-                } else {
-                    $("#currentPage").empty();
-                    displaySearch(e.target.result);
-                    }
-            })
+                searchResult = [];
+                searchResult = e.target.result;
+                createSearchResults();
+            });
         })
-        });
+    });
+
     $("#searchName").click(function (event) {
         $("#searchByWhat").click(function (event) {
             var search = document.getElementById("searchBy").value;
@@ -45,16 +37,10 @@ $(document).ready(function () {
             var result =  get_data_name(search);
             result.then((e) => {
                 console.log(e.target.result);
-                if (e.target.result.length === 0 || e.target.result.length === undefined) {
-                    $("#currentPage").text("No pages found!");
-                } else {
-                    $("#currentPage").empty();
-                    for (i = 0; i < e.target.result.length; i++) {
-                        var p = e.target.result[i];
-                        displaySearch(p);
-                    }
-                }
-            })
+                searchResult = [];
+                searchResult = e.target.result;
+                createSearchResults();
+            });
         })
         });
     $("#searchAuthor").click(function (event) {
@@ -64,17 +50,10 @@ $(document).ready(function () {
             var result = get_data_author(search);
             result.then((e) => {
                 console.log(e.target.result);
-                if (e.target.result.length === 0 || e.target.result.length === undefined) {
-                    $("#currentPage").text("No pages found!");
-                } else {
-                    $("#currentPage").empty();
-                    for (i = 0; i < e.target.result.length; i++) {
-                        var p = e.target.result[i];
-                        console.log(p);
-                        displaySearch(p);
-                    }
-                }
-            })
+                searchResult = [];
+                searchResult = e.target.result;
+                createSearchResults();
+            });
         })
     })
 
@@ -88,34 +67,42 @@ function searchFor(search) { //search comes from button listeners above
         console.log(e.target.result);
         searchResult = [];
         searchResult = e.target.result;
-        if (e.target.result.length == 0 || e.target.result.length === undefined) {
-            $("#currentPage").text("No pages found!");
-        }
-        else {
-            $("#currentPage").empty();
-            for (i = 0; i < e.target.result.length; i++) {
-                var p = e.target.result[i]
-                displaySearch(p, i);
-            }
-        }
-        var viewButtons = document.querySelectorAll("div.pageDetails > .viewButton");
-
-
-        console.log(viewButtons);
-        for (var j = 0; j < viewButtons.length; j++) {
-            viewButtons[j].addEventListener('click', function (event) {
-                var viewButtonName = event.target.name.slice(5);
-                var vbnInt = parseInt(viewButtonName);
-                var actualPage = searchResult[vbnInt].html;
-                $("#currentPage").empty();
-                console.log(viewButtonName);
-                $("#localPage").text(searchResult[vbnInt].name);
-                $("#currentPage").html(actualPage);
-
-            })
-        }
-
+        createSearchResults();
     });
+}
+
+function createSearchResults() {
+    if (searchResult == 0 || searchResult === undefined) {
+        $("#currentPage").text("No pages found!");
+    }
+    else {
+        $("#currentPage").empty();
+        for (i = 0; i < searchResult.length; i++) {
+            var p = searchResult[i]
+            displaySearch(p, i);
+
+        }
+        createButtonListeners();
+    }
+}
+
+function createButtonListeners() {
+    var viewButtons = document.querySelectorAll("div.pageDetails > .viewButton");
+    //var editButtons = document.querySelectorAll("div.pageDetails > .editButton");
+
+    console.log(viewButtons);
+    for (var j = 0; j < viewButtons.length; j++) {
+        viewButtons[j].addEventListener('click', function (event) {
+            var viewButtonName = event.target.name.slice(5);
+            var vbnInt = parseInt(viewButtonName);
+            var actualPage = searchResult[vbnInt].html;
+            $("#currentPage").empty();
+            console.log(viewButtonName);
+            $("#localPage").text(searchResult[vbnInt].name);
+            $("#currentPage").html(actualPage);
+
+        })
+    }
 }
 
 function toEditor(editor_url) {
@@ -133,17 +120,21 @@ function makeURL(singlePage) {
 
 //basic display of all search items, will be cleaned up later
 function displaySearch(singlePage, pageArrayNumber) {
+    //this function is iterated over for each object in the search result array
     let comment;
     let editor_url = makeURL(singlePage);
     if (singlePage.comment === undefined) {
-        comment = "There is no comments for this page"
+        comment = "There are no comments for this page"
     } else {
         comment = singlePage.comment;
     }
 
     var stringEntry = "<h2>" + "<a href=" + editor_url + ">" + singlePage.name + "</a>" + "</h2>" + "<p> <i>" + comment + "</i> <br> Last edited online by " + singlePage.author.name + "</p>";
-    var stringEntry1= "<p>OID: " + singlePage.oid +"</p>"
-    $("#currentPage").append("<div class='pageDetails'>" + stringEntry + stringEntry1 + "<button name='view-" + pageArrayNumber + "' type='button' class='viewButton'>View</button>" + "</div>");
+    var stringEntry1 = "<p>OID: " + singlePage.oid + "</p>";
+    var viewEntry = "<button name='view-" + pageArrayNumber + "' type='button' class='viewButton'>View</button>";
+    var editEntry = "<a href=" + editor_url + ">Edit</a>";
+
+    $("#currentPage").append("<div class='pageDetails'>" + stringEntry + stringEntry1 + viewEntry + editEntry +"</div>");
 }
 
 function noHomepage() {
