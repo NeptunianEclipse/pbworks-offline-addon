4b281f1168f2307b589ad3653202fdfd66d8af12
Lauren <39478077+SerendipitousUser@users.noreply.github.com>
2019-10-14 10:35:57 +1300

    Slightly changed wording and paragraph appearance
    - Cleaned up the appearance of paragraphs when viewing pages

--- popup/JS/LoadLocalPage.js
+++ popup/JS/LoadLocalPage.js
@@ -39,6 +39,7 @@ $(document).ready(function () {
                 $("#currentPage").html("<h2>You haven't downloaded a frontpage!</h2>");
             } else {
                 $("#currentPage").html(e.target.result[0].html);
+                $('#currentPage p').after('<br/>');
             }
         });
 
@@ -351,6 +352,7 @@ $(document).ready(function () {
                     }
 
                 }
+                $('#currentPage p').after('<br/>');
             })
         }
     }
@@ -382,7 +384,7 @@ $(document).ready(function () {
 
         var stringEntry = "<h2>" + "<a href=" + editor_url + ">" + singlePage.name + "</a> </h2>" + "<p> <i>" + comment + "</i> <br> Last edited online by " + singlePage.author.name + "</p>";
         var stringEntry1 = "<br><p>OID: " + singlePage.oid + "</p>";
-        var editTime = "<p>last modify time: " + singlePage.edittime + "</p>";
+        var editTime = "<p>Last Modified: " + singlePage.edittime + "</p>";
         var viewEntry = "<button name='view-" + pageArrayNumber + "' type='button' class='viewButton'>View</button>";
         var editEntry = '<a href=' + editor_url + ' class="editPage">Edit</a>';
 
